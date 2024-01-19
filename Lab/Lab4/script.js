dc6566d1ea80fe085f8235cbb95b7bac1bc60ab7
dinhkhiem <khiemddpc05680@fpt.edu.vn>
2024-01-19 19:51:22 +0700

    Lab4

--- Lab/Lab4/script.js
+++ Lab/Lab4/script.js
@@ -78,26 +78,73 @@
 //     console.log("10s");
 // },10000)
 
-const axios = require('axios').default;
+// const axios = require('axios').default;
 
-const API_URL = "http://localhost:3000/";
+// const API_URL = "http://localhost:3000/";
 
-// lay danh sach 
-axios.get(API_URL+ 'comments').then(({data})=>console.log(data))
+// // lay danh sach 
+// axios.get(API_URL+ 'comments').then(({data})=>console.log(data))
 
-//lay1 phan tu
-axios.get(API_URL+ 'comments/'+1).then(({data})=>console.log(data))
-let comment = {
-    "body":"Bai viet qa hay",
-    "postId":1    
-}
-//them moi
-axios.post(API_URL+ 'comments',comment).then(({data})=>console.log(data))
+// //lay1 phan tu
+// axios.get(API_URL+ 'comments/'+1).then(({data})=>console.log(data))
+// let comment = {
+//     "body":"Bai viet qa hay",
+//     "postId":1    
+// }
+// //them moi
+// axios.post(API_URL+ 'comments',comment).then(({data})=>console.log(data))
 
-axios.get(API_URL+ 'comments').then(({data})=>console.log(data))
+// axios.get(API_URL+ 'comments').then(({data})=>console.log(data))
 
-// sua 
-axios.put(API_URL+ 'comments/'+2,comment).then(({data})=>console.log(data))
+// // sua 
+// axios.put(API_URL+ 'comments/'+2,comment).then(({data})=>console.log(data))
 
-//xoa 
-axios.delete(API_URL+ 'comments/'+2).then(({data})=>console.log(data))
+// //xoa 
+// axios.delete(API_URL+ 'comments/'+2).then(({data})=>console.log(data))
+
+
+//lab4 bài 1 
+let promise = new Promise((resolve,reject)=>{
+    resolve(1);
+    setTimeout(() => resolve(2),100);
+})
+promise.then(alert) // kq là 1
+
+//lab4 bài 2 
+// const axios = require('axios');
+// doan code 1 
+// async function fetchUrls(urls){
+//     const results = [];
+//     for(const url of urls){
+//         const res = await axios.get(url);
+//         results.push(res);
+//     }
+//     return results;
+// }
+
+// doan code 2 
+// async function fetchUrlsParallel(urls){
+//     const results = await Promise.all(
+//         urls.map(function(url){
+//             return axios.get(url);
+//         })
+//     );
+//     return results;
+// }
+//Đoạn code 1 sử dụng vòng lặp for để duyệt qua danh sách các URL, trong khi đoạn code 2 sử dụng phương thức map() của JavaScript.
+//Đoạn code 1 sử dụng hàm axios.get() để lấy dữ liệu từ các URL, trong khi đoạn code 2 sử dụng phương thức axios.all() để lấy dữ liệu từ tất cả các URL cùng một lúc.
+
+// lab4 bai 3
+// const fs = require('fs');
+// const axios = require('axios');
+
+// async function main() {
+//   const data = await fs.readFile('./data.json', 'utf8');
+//   const res = await axios.get('https://jsonplaceholder.typicode.com/todos/1');
+
+//   console.log('Data loaded from disk', data);
+//   console.log('Data downloaded from url', res.data);
+// }
+
+// main();
+
