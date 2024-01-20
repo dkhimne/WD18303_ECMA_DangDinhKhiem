add2617e35d38a6d0871d4d17042c54d42ad1750
dinhkhiem <khiemddpc05680@fpt.edu.vn>
2024-01-20 18:47:59 +0700

    .

--- Assignment/Client/js/data.js
+++ Assignment/Client/js/data.js
@@ -1,16 +1,22 @@
-const response = await fetch("./db.json");
-const data = await response.json();
+// Lấy dữ liệu sản phẩm từ file JSON
+const getProducts = () => {
+  const url = `db.json`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data);
+};
 
-let html = document.getElementById('product');
+// Hiển thị dữ liệu sản phẩm lên giao diện
+const renderProducts = (products) => {
+  const productContainer = document.querySelector(".products");
+  products.forEach(product => {
+    const productItem = document.createElement("div");
+    productItem.classList.add("product-item");
 
-products.forEach(element => {                                
-  let row = `
-    <div>
-      <div><img src="${element.image}"/></div>
-      <div>${element.cate_id}</div>
-      <h4>${element.name}</h4>
-      <div>${element.price}</div>
-    </div>`;  
-
-  html.innerHTML += row;
-});
+    // Tạo phần tử ảnh sản phẩm
+    const productImage = document.createElement("img");
+    productImage.src = product.image;
+    productImage.classList.add("product-image");
+    productImage.alt = product.name;
+  });
+}
