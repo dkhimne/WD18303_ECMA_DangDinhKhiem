54c98e56cb1dba5d249769345a7b055de2f36874
dinhkhiem <khiemddpc05680@fpt.edu.vn>
2024-01-20 12:06:19 +0700

    .

--- Assignment/Client/js/data.js
+++ Assignment/Client/js/data.js
@@ -1,2 +1,16 @@
 const response = await fetch("./db.json");
 const data = await response.json();
+
+let html = document.getElementById('product');
+
+products.forEach(element => {                                
+  let row = `
+    <div>
+      <div><img src="${element.image}"/></div>
+      <div>${element.cate_id}</div>
+      <h4>${element.name}</h4>
+      <div>${element.price}</div>
+    </div>`;  
+
+  html.innerHTML += row;
+});
