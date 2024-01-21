42d0d3c31f097ebc5c7c25aca64f802c77d61b0f
dinhkhiem <khiemddpc05680@fpt.edu.vn>
2024-01-21 16:01:29 +0700

    .

--- Assignment/Client/js/data.js
+++ Assignment/Client/js/data.js
@@ -6,14 +6,14 @@ fetch("db.json")
       
     product.forEach(product => {
       html.innerHTML += `
-        <div class="rounded position-relative fruite-item">
+        <div class="rounded position-relative fruite-item" >
           <div class="fruite-img">
            <a href="shop-detail.html"> <img src="${product.image}" class="img-fluid w-100 rounded-top" alt=""> </a>
           </div>
           <div class="text-white bg-secondary px-3 py-1 rounded position-absolute" style="top: 10px; left: 10px;">${product.cate_name}</div>
           <div class="p-4 border border-secondary border-top-0 rounded-bottom">
             <h4>${product.name}</h4>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit sed do eiusmod te incididunt</p>
+            <p>San pham huu co khong phan bon</p>
             <div class="d-flex justify-content-between flex-lg-wrap">
               <p class="text-dark fs-5 fw-bold mb-0">${product.price}</p>
               <a href="#" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
@@ -24,3 +24,4 @@ fetch("db.json")
       });
     });
   });
+  
