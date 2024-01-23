aa04f2790656b87ecaa85c202b22e613ed6231e9
dinhkhiem <khiemddpc05680@fpt.edu.vn>
2024-01-23 22:19:54 +0700

    Update

--- Assignment/Admin/pages/tables/data.js
+++ Assignment/Admin/pages/tables/data.js
@@ -1,18 +1,69 @@
-fetch("db.json")
+//lisst product
+fetch("http://localhost:3000/products")
     .then((response) => {
         response.json().then((data) => {
-            const products = data.products;
+            const products = data;
             const html = document.querySelector('#san-pham tbody');
 
             products.forEach(product => {
                 html.innerHTML += `
                     <tr>
                         <td class="py-1">
-                            <img src="${product.image}" alt="image"/>
+                            <img src="${product.image}" alt="image"  style="width: 100px;height: 100px;"/>
                         </td>
                         <td>${product.cate_name}</td>
                         <td>${product.name}</td>
                         <td>${product.price}</td>
+                        <td>
+                        <button type="button" class="btn btn-primary mt-3"  ">Edit </button>
+                        <button type="button" class="btn btn-danger mt-3"  ">Delete</button>
+                        </td>
+                    </tr>
+                `;
+            });
+        });
+    });
+
+
+//list cate
+    fetch("http://localhost:3000/categories")
+    .then((response) => {
+        response.json().then((data) => {
+            const categories = data;
+            const html = document.querySelector('#danh-muc tbody');
+
+            categories.forEach(cate => {
+                html.innerHTML += `
+                    <tr>                        
+                        <td>${cate.id}</td>
+                        <td>${cate.name}</td>
+                        <td>
+                        <button type="button" class="btn btn-primary mt-3"  ">Edit </button>
+                        <button type="button" class="btn btn-danger mt-3"  ">Delete</button>
+                        </td>
+                    </tr>
+                `;
+            });
+        });
+    });
+
+//list order
+fetch("http://localhost:3000/orders")
+    .then((response) => {
+        response.json().then((data) => {
+            const orders = data;
+            const html = document.querySelector('#don-hang tbody');
+
+            orders.forEach(or => {
+                html.innerHTML += `
+                    <tr>                        
+                        <td>${or.id}</td>
+                        <td>${or.customer_name}</td>
+                        <td>${or.customer_address}</td>
+                        <td>${or.customer_email}</td>
+                        <td>${or.customer_phone_number}</td>
+                        <td>${or.created_date}</td>
+                        <td>${or.status}</td>
                     </tr>
                 `;
             });
--- Assignment/Client/js/data.js
+++ Assignment/Client/js/data.js
@@ -1,11 +1,14 @@
+//index va shop
 fetch("db.json")
-  .then((response) =>{
+  .then((response) => {
     response.json().then((data) => {
-    const product = data.products ;
-    const html = document.getElementById('san-pham');
-      
-    product.forEach(product => {
-      html.innerHTML += `
+      const product = data.products;
+      const html = document.getElementById('san-pham');
+
+      product.forEach(product => {
+        html.innerHTML += `
+        
+        <div class="col-md-6 col-lg-6 col-xl-4">
         <div class="rounded position-relative fruite-item" >
           <div class="fruite-img">
            <a href="shop-detail.html"> <img src="${product.image}" class="img-fluid w-100 rounded-top" alt=""> </a>
@@ -19,9 +22,47 @@ fetch("db.json")
               <a href="cart.html" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
             </div>
           </div>
-        </div>
+          </div>
+          </div>
       `;
       });
     });
   });
-  
+
+
+//best seller
+ 
+  fetch("db.json")
+  .then((response) => {
+    response.json().then((data) => {
+      const product = data.products;
+      const html = document.getElementById('best-seller');
+
+      product.forEach(product => {
+        html.innerHTML += `   
+        <div class="col-lg-6 col-xl-4">
+                    <div class="p-4 rounded bg-light">
+                        <div class="row align-items-center" >     
+        <div class="col-6">
+        <img src="${product.image}" class="img-fluid rounded-circle w-100" alt="">
+        </div>
+        <div class="col-6">
+        <a href="#" class="h5">${product.name}</a>
+        <div class="d-flex my-3">
+          <i class="fas fa-star text-primary"></i>
+          <i class="fas fa-star text-primary"></i>
+          <i class="fas fa-star text-primary"></i>
+          <i class="fas fa-star text-primary"></i>
+          <i class="fas fa-star"></i>
+          </div>
+          <h4 class="mb-3">${product.price}</h4>
+         <a href="cart.html" class="btn border border-secondary rounded-pill px-3 text-primary"><i class="fa fa-shopping-bag me-2 text-primary"></i> Add to cart</a>
+        </div>
+        </div>
+       </div>
+       </div>   
+      `;
+      });
+    });
+  });
+
